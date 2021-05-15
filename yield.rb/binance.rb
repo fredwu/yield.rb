c06139ee0ae1481b24262678b91da70f7a695b0b
Fred Wu <ifredwu@gmail.com>
2021-05-16 00:28:53 +1000

    Reverted back to using the Binance account snapshot API

--- yield.rb/binance.rb
+++ yield.rb/binance.rb
@@ -1,8 +1,10 @@
 class Binance < Exchange
-  API_URI = "https://api.binance.com/api/v3/account"
+  # uses the Binance account snapshot API as the account API does not include
+  # data on the locked staking positions
+  API_URI = "https://api.binance.com/sapi/v1/accountSnapshot"
 
   def parse
-    data["balances"].reject do |balance|
+    data["snapshotVos"].last["data"]["balances"].reject do |balance|
       balance["asset"] =~ /\w+(UP|DOWN)$/ || balance["free"].to_f == 0
     end.map do |balance|
       { token_name(balance["asset"]) => balance["free"].to_f }
@@ -13,7 +15,7 @@ class Binance < Exchange
 
   def get_from_api(api_key, secret_key)
     timestamp = Time.now.to_i * 1000
-    params    = "timestamp=#{timestamp}"
+    params    = "type=SPOT&timestamp=#{timestamp}"
     signature = OpenSSL::HMAC.hexdigest("sha256", secret_key, params)
     params    = "#{params}&signature=#{signature}"
     uri       = URI("#{API_URI}?#{params}")
