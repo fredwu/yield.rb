32cfe038da9875230c958ed4b52383265083e13b
Fred Wu <ifredwu@gmail.com>
2021-05-31 12:08:17 +1000

    Updated rounding

--- yield.rb/manual.rb
+++ yield.rb/manual.rb
@@ -10,11 +10,11 @@ class Manual
   def parse
     data.map do |group, group_data|
       group_data.map do |token, amount|
-        { token => amount.round(rounding) }
+        { token => amount }
       end
     end.flatten.inject do |memo, el|
       memo.merge(el) do |token, amount, new_amount|
-        amount + new_amount
+        (amount + new_amount).round(rounding)
       end
     end
   end
